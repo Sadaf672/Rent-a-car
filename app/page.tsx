e00fe4d9f1070a4396b6c17f030cc7578028715b
Sadaf672 <sadafkhankhan559@gmail.com>
2024-12-26 23:34:39 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+import PickDropForm from './components/PickDropForm'
+import Categories from './components/Catagore';  
+
+const App = () => {
+  return (
+    <div>
+      <HeroSection />
+      <PickDropForm />
+      <Categories />
+       
+      
+    </div>
+  )
+}
+
+export default App
